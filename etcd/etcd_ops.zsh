de88054a901080258a770b4cbfc6900c04748d1e
liuxuc63 <liuxuc63@163.com>
2022-12-18 13:50:19 +0800

    Update etcd_ops.zsh

--- etcd/etcd_ops.zsh
+++ etcd/etcd_ops.zsh
@@ -1,3 +1,11 @@
+# ----- Reset the only last one member ----
+force-new-cluster: true # /etc/etcd/config.yaml
+# Force to create a new one-member cluster. It commits configuration changes forcing to remove all existing members in the cluster and add itself. It needs to be set to restore a backup.
+# default: false
+# env variable: ETCD_FORCE_NEW_CLUSTER
+
+
+
 # put value
 etcdctl put /dns/dc 0
 etcdctl put /dns/dc/dev/mq1 '{"host":"172.17.0.3","ttl":10}'
